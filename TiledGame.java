5fad815a26fa67920105446ba0b0bb03d6835b85
Jodan Elysee <75323379+jodanelysee@users.noreply.github.com>
2023-12-23 16:32:01 -0500

    Java file to set up game window

---
+++ TiledGame.java
@@ -0,0 +1,31 @@
+package com.blooddrive.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class TiledGame extends Game {
+    // We'll set the game window size in the Game subclass object;
+    // the DesktopLauncher will use these constants when launching the game
+    public static final int GAMEWIDTH = 800;
+    public static final int GAMEHEIGHT = 480;
+
+    public SpriteBatch batch;
+    public BitmapFont font;
+
+    public void create() {
+        batch = new SpriteBatch();
+        //Use LibGDX's default Arial font.
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    public void render() {
+        super.render(); //important!
+    }
+
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+}
